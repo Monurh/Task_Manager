a211ed92b6bd36dfcab0386922bd8e4cd567237c
Monurh <ebelous581@gmail.com>
2023-12-12 20:35:24 +0200

    add

---
+++ Task_Manager/Model/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_Manager.Model
+{
+    public class Task
+    {
+        public Guid TaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueData { get; set; }
+        public int Priority { get; set; }
+        public string Status { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Task_Manager/Model/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_Manager.Model
+{
+    public class Users
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
